3b69a5c025d01d3012597d9f4d4a7dd3c18114b1
Konstantin Kapelnikov <reg_kapelnikovka@mail.ru>
2017-09-10 11:19:41 +0400

    Правки
    Единообразие кода и пиксель перфект

--- js/script.js
+++ js/script.js
@@ -6,23 +6,23 @@ var modalForm = modalModalPopup.querySelector("form");
 var storage = localStorage.getItem("name");
 
 modalBtnOpen.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modalModalPopup.classList.add("write-us-form-wrapper-show");
-  modalFocus.focus();
+    evt.preventDefault();
+    modalModalPopup.classList.add("write-us-form-wrapper-show");
+    modalFocus.focus();
 });
 
 modalBtnClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modalModalPopup.classList.remove("write-us-form-wrapper-show");
-  modalModalPopup.classList.remove("write-us-error");
+    evt.preventDefault();
+    modalModalPopup.classList.remove("write-us-form-wrapper-show");
+    modalModalPopup.classList.remove("write-us-error");
 });
 
 modalForm.addEventListener("submit", function(evt) {
-	if (!modalFocus.value) {
-		evt.preventDefault();
-		modalModalPopup.classList.add("write-us-error");
-	} else {
-		localStorage.setItem("name", modalFocus.value);
-	}
-	 
+    if (!modalFocus.value) {
+        evt.preventDefault();
+        modalModalPopup.classList.add("write-us-error");
+    } else {
+        localStorage.setItem("name", modalFocus.value);
+    }
+
 });
